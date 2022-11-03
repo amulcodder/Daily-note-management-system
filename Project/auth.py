777a501b156848bf2ebe36ebf90624ac2dbbd090
Amul_Soni <amulvxi@gmail.com>
2022-11-03 17:42:05 +0530

    Make it Better

--- Project/auth.py
+++ Project/auth.py
@@ -1,10 +1,8 @@
 import string
-
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from wsgiref.validate import validator
 import validators
 from flask_login import login_user, login_required, logout_user, current_user
 
